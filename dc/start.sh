805834521e8125aa50a315460b0d67b6fb56e438
fox <80005368@qq.com>
2019-11-08 15:09:04 +0800

    数据目录

--- dc/start.sh
+++ dc/start.sh
@@ -7,6 +7,8 @@ mkdir -p ${DIR}/elasticsearch/data
 mkdir -p ${DIR}/elasticsearch/logs
 
 mkdir -p ${DIR}/logstash/logs
+mkdir -p ${DIR}/logstash/data
+mkdir -p ${DIR}/logstash/pipeline
 
 
 chmod -R 777 ${DIR}/elasticsearch/data
@@ -14,6 +16,7 @@ chmod -R 777 ${DIR}/elasticsearch/logs
 
 chmod -R 777 ${DIR}/logstash/pipeline
 chmod -R 777 ${DIR}/logstash/logs
+chmod -R 777 ${DIR}/logstash/data
 
 
 docker-compose up -d
